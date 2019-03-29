9829cd8346fc975f94f996ec7255c7e527b33702
Jeehyeop-kwon <jeehyeop@naver.com>
2019-03-29 13:03:54 -0400

    Add files via upload

---
+++ StringLab.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStringReverse
+{
+    public class StringLab
+    {
+        public StringLab()
+        { }
+        public StringLab(string forwardString)
+        {
+            originalString = forwardString;
+        } 
+
+        //string[] blackList = {"shit","save","coupon" };
+
+        private string originalString;
+        public string OriginalString
+        {
+            get { return originalString; } 
+            set
+            {
+                if (value.ToLower().Contains("shit"))
+                {
+                    originalString = "Please try again.";
+                }
+                else
+                { 
+                    originalString = value;
+                } 
+            }
+        }
+
+
+        public string StringReverse()
+        { 
+            StringBuilder sb = new StringBuilder();
+            sb.Append("You have to work on this method to make it work.\n");
+            sb.Append(originalString);
+            return sb.ToString();
+        }
+
+
+
+    }
+}
